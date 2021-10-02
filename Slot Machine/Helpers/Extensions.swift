9847483982b618dff00c661d3ed8372fda6b8e14
Nic Deane <njdeane7@gmail.com>
2021-10-02 18:17:40 +1000

    Create top part of ContentView.

---
+++ Slot Machine/Helpers/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  Slot Machine
+//
+//  Created by Nic Deane on 2/10/21.
+//
+
+import SwiftUI
+
+extension Text {
+  func scoreLabelStyle() -> Text {
+    self
+      .foregroundColor(Color.white)
+      .font(.system(size: 10, weight: .bold, design: .rounded))
+  }
+  
+  func scoreNumberStyle() -> Text {
+    self
+      .foregroundColor(Color.white)
+      .font(.system(.title, design: .rounded))
+      .fontWeight(.heavy)
+  }
+}
--- Slot Machine/View/ContentView.swift
+++ Slot Machine/View/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  
+  
+  
+  var body: some View {
+    ZStack {
+      // BACKGROUND
+      LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
+      
+      // INTERFACE
+      VStack(alignment: .center, spacing: 5) {
+        // HEADER
+        LogoView()
+        
+        Spacer()
+        
+        // SCORE
+        HStack {
+          HStack {
+            Text("Your\nCoins".uppercased())
+              .scoreLabelStyle()
+              .multilineTextAlignment(.trailing)
+            
+            Text("100")
+              .scoreNumberStyle()
+              .modifier(ScoreNumberModifier())
+          }
+          .modifier(ScoreContainerModifier())
+          
+          Spacer()
+          
+          HStack {
+            Text("200")
+              .scoreNumberStyle()
+              .modifier(ScoreNumberModifier())
+            
+            Text("High\nScore".uppercased())
+              .scoreLabelStyle()
+              .multilineTextAlignment(.leading)
+          }
+          .modifier(ScoreContainerModifier())
+        }
+        
+        // SLOT MACHINE
+        
+        // FOOTER
+        Spacer()
+      }
+      // BUTTONS
+      .overlay(
+        Button(action: {
+          print("Reset the game")
+        }) {
+          Image(systemName: "arrow.2.circlepath.circle")
+        }
+          .modifier(ButtonModifier()),
+        alignment: .topLeading
+      )
+      .overlay(
+        Button(action: {
+          print("info")
+        }) {
+          Image(systemName: "info.circle")
+        }
+          .modifier(ButtonModifier()),
+        alignment: .topTrailing
+      )
+      .padding()
+      .frame(maxWidth: 720)
+      
+      // POPUP
+    } //: Zstack
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
