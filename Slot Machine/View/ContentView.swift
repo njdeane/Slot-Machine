0185bb652319c74b6c3b4fffecacd11c15d919b0
Nic Deane <njdeane7@gmail.com>
2021-10-04 17:25:07 +1000

    Game logic done. Now just needs animations.

--- Slot Machine/View/ContentView.swift
+++ Slot Machine/View/ContentView.swift
@@ -11,11 +11,14 @@ struct ContentView: View {
   
   let symbols = ["gfx-bell", "gfx-cherry", "gfx-coin", "gfx-grape", "gfx-seven", "gfx-strawberry"]
   
-  @State private var highscore: Int = 0
+  @State private var highscore: Int = UserDefaults.standard.integer(forKey: "HighScore")
   @State private var coins: Int = 100
   @State private var betAmount: Int = 10
   @State private var reels: Array = [0,1,2]
   @State private var showingInfoView: Bool = false
+  @State private var isBet10: Bool = true
+  @State private var isBet20: Bool = false
+  @State private var showingModal: Bool = false
 
   func spinReels() {
     reels = reels.map({ _ in
@@ -40,11 +43,34 @@ struct ContentView: View {
   
   func newHighScore() {
     highscore = coins
+    UserDefaults.standard.set(highscore, forKey: "HighScore")
   }
   
   func playerLoses() {
     coins -= betAmount
   }
+  
+  func activateBet20() {
+    betAmount = 20
+  }
+  
+  func activateBet10() {
+    betAmount = 10
+  }
+  
+  func isGameOver() {
+    if coins <= 0 {
+      showingModal = true
+    }
+  }
+  
+  func resetGame() {
+    UserDefaults.standard.set(0, forKey: "HighScore")
+    highscore = 0
+    coins = 100
+    activateBet10()
+  }
+  
   
   var body: some View {
     ZStack {
@@ -123,8 +149,9 @@ struct ContentView: View {
           
           // Spin Button
           Button(action: {
-            self.spinReels() // why self?
+            self.spinReels()
             self.checkWinning()
+            self.isGameOver()
           }) {
             Image("gfx-spin")
               .renderingMode(.original)
@@ -141,11 +168,13 @@ struct ContentView: View {
         HStack {
           HStack(alignment: .center, spacing: 10) {
             Button(action: {
-              print("Bet 20 coins")
+              activateBet20()
+              isBet20 = true
+              isBet10 = false
             }) {
               Text("20")
                 .fontWeight(.heavy)
-                .foregroundColor(Color.white)
+                .foregroundColor(isBet20 ? Color("ColorYellow") : Color.white)
                 .modifier(BetNumberModifier())
             }
             .modifier(BetCapsuleModifier())
@@ -153,22 +182,24 @@ struct ContentView: View {
             
             Image("gfx-casino-chips")
               .resizable()
-              .opacity(0)
+              .opacity(isBet20 ? 1 : 0)
               .modifier(CasinoChipsModifier())
           }
           
           HStack(alignment: .center, spacing: 10) {
             Image("gfx-casino-chips")
               .resizable()
-              .opacity(1)
+              .opacity(isBet10 ? 1 : 0)
               .modifier(CasinoChipsModifier())
             
             Button(action: {
-              print("Bet 10 coins")
+              activateBet10()
+              isBet10 = true
+              isBet20 = false
             }) {
               Text("10")
                 .fontWeight(.heavy)
-                .foregroundColor(Color.yellow)
+                .foregroundColor(isBet10 ? Color("ColorYellow") : Color.white)
                 .modifier(BetNumberModifier())
             }
             .modifier(BetCapsuleModifier())
@@ -179,7 +210,7 @@ struct ContentView: View {
       // BUTTONS
       .overlay(
         Button(action: {
-          print("Reset the game")
+          self.resetGame()
         }) {
           Image(systemName: "arrow.2.circlepath.circle")
         }
@@ -197,8 +228,67 @@ struct ContentView: View {
       )
       .padding()
       .frame(maxWidth: 720)
+      .blur(radius: $showingModal.wrappedValue ? 5 : 0, opaque: false)
       
       // POPUP
+      if $showingModal.wrappedValue {
+        ZStack {
+          Color("ColorTransparentBlack").edgesIgnoringSafeArea(.all) // this view prevents user tappnig on the blurred area from working
+          
+          VStack(spacing: 0) {
+            Text("GAME OVER")
+              .font(.system(.title, design: .rounded))
+              .fontWeight(.heavy)
+              .padding()
+              .frame(minWidth: 0, maxWidth: .infinity)
+              .background(Color("ColorPink"))
+              .foregroundColor(Color.white)
+            
+            Spacer()
+            
+            VStack(alignment: .center, spacing: 16) {
+              Image("gfx-seven-reel")
+                .resizable()
+                .scaledToFit()
+                .frame(maxHeight: 72)
+              
+              Text("Bad luck! You lost all of the coins. \nLet's play again!")
+                .font(.system(.body, design: .rounded))
+                .lineLimit(2)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.gray)
+                .layoutPriority(1)
+              
+              Button(action: {
+                self.showingModal = false
+                self.coins = 100
+              }) {
+                Text("New Game".uppercased())
+                  .font(.system(.body, design: .rounded))
+                  .fontWeight(.semibold)
+                  .accentColor(Color("ColorPink"))
+                  .padding(.horizontal, 12)
+                  .padding(.vertical, 8)
+                  .frame(minWidth: 128)
+                  .background(
+                    Capsule()
+                      .stroke(lineWidth: 1.75)
+                      .foregroundColor(Color("ColorPink"))
+                  )
+              }
+
+            }
+            
+            Spacer()
+          }
+          .frame(minWidth: 280, idealWidth: 280, maxWidth: 320, minHeight: 260, idealHeight: 280, maxHeight: 320, alignment: .center)
+          .background(Color.white)
+          .cornerRadius(20)
+          .shadow(color: Color("ColorTransparentBlack"), radius: 6, x: 0, y: 8)
+        }
+      }
+      
+      
     } //: Zstack
     .sheet(isPresented: $showingInfoView) {
       InfoView()
